72553105931afa2d4b027351cbfb75d48f0b9f28
oyat2bit <oyat2bit@gmail.com>
2022-06-01 11:09:39 +0900

    add Xcode and Podfile

---
+++ SeoulSubwayAPI/SeoulSubwayAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeoulSubwayAPI
+//
+//  Created by jamescode on 2022/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
